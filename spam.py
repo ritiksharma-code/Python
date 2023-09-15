2a8c56914cc9ef47725aff39ae73d8e8a6cdd6a3
ritiksharma-code <ritiksharma0405@gmail.com>
2023-09-15 14:35:42 +0530

    update

---
+++ spam.py
@@ -0,0 +1,9 @@
+import random 
+import pyautogui as pg
+import time
+#animal=('meethe','chote bhai','chutiye')
+time.sleep(8)
+for i in range(1000):
+    #a=random.choice(animal)
+    pg.write("laude")
+    pg.press('enter')
